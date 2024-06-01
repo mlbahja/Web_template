b28d80ef00a2b1c1101428bb889646e6b60b64a0
0696250248@m <mlbahja28@gmail.com>
2024-06-01 16:13:25 +0100

    test

---
+++ tet.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+func pageweb(w http.ResponseWriter, r *http.Request) {
+	templatepage, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "400: Bad request.", http.StatusBadRequest)
+		return
+	}
+	nbr1, _ := strconv.Atoi(r.FormValue("NUM1"))
+	nbr2, _ := strconv.Atoi(r.FormValue("NUM2"))
+	result := nbr1 + nbr2
+	templatepage.Execute(w, result)
+}
+
+func main() {
+	http.HandleFunc("/", pageweb)
+	http.ListenAndServe(":8080", nil)
+}
