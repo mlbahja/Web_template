3ca4ef75ad9e27b99d081221f527d4ddda518da6
Mustapha Lbahja <112217233+mlbahja@users.noreply.github.com>
2024-07-10 17:07:50 +0100

    Update tet.go

--- tet.go
+++ tet.go
@@ -12,8 +12,8 @@ func pageweb(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "400: Bad request.", http.StatusBadRequest)
 		return
 	}
-	nbr1, _ := strconv.Atoi(r.FormValue("NUM1"))
-	nbr2, _ := strconv.Atoi(r.FormValue("NUM2"))
+	nbr1, _ := strconv.Atoi(r.FormValue("num1"))
+	nbr2, _ := strconv.Atoi(r.FormValue("num2"))
 	result := nbr1 + nbr2
 	templatepage.Execute(w, result)
 }
